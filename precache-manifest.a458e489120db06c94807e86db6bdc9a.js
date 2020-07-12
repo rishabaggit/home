0744d5b804e75b6715e3b775b7ddd363956e05db
rishabaggit <rishabag@iitk.ac.in>
2020-07-13 02:06:09 +0530

    Updates

---
+++ precache-manifest.a458e489120db06c94807e86db6bdc9a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "918fea7dd40733cb4d22941ae1df6043",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "95ea5c2ae10eb35bb877",
+    "url": "/home/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "fd878f86ee128da3a6e9",
+    "url": "/home/static/css/main.5f05969b.chunk.css"
+  },
+  {
+    "revision": "95ea5c2ae10eb35bb877",
+    "url": "/home/static/js/2.448dd0be.chunk.js"
+  },
+  {
+    "revision": "b71cb3cccc73e099d412c24a04bf9105",
+    "url": "/home/static/js/2.448dd0be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd878f86ee128da3a6e9",
+    "url": "/home/static/js/main.9dc48765.chunk.js"
+  },
+  {
+    "revision": "5c07ecca0456e28e04f0",
+    "url": "/home/static/js/runtime-main.e084aacb.js"
+  },
+  {
+    "revision": "2ab07aecf001c50d5cae80e042d8ca20",
+    "url": "/home/static/media/resume.2ab07aec.pdf"
+  }
+]);
